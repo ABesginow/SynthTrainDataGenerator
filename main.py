8744dcccd49c138f0adfa6dee304bb9d879ed435
andreas <andreas.besginow@hs-owl.de>
2020-09-03 17:03:40 +0200

    first lines for using cfg file

--- main.py
+++ main.py
@@ -1,56 +1,34 @@
-"""
-
-Main file
-Purpose:
--> Executes the main loop where images are taken and given to threads to execute edge detection and contour detection
-
-
-
-
-structure of main loop:
-
->initialise variables
-|>capture image
-|>start thread
-||>increase contrast
-||>run edge detection
-||>determine largest contour of the object
-||>determine outer box of largest contour
-||>safe image to correct folder
-||>write information in txt file in correct yolo format
-|>move motor --> loop back to image capture
->loop is finished as soon as 12360 iterations have been executed (1 full circle)
->write everything into files (image names, txt names etc.)
->zip everything
->send everything over network to alienware
->remote start script on alienware
->wait
-"""
+#Notes: 1 full circle is 12360 movements of motor
 
 from image_capture import ImageCapture
 from motor_control_PI import MotorControl
 from image_processing import ImageProcessing
 from file_operations import FileOperations
-#from CLI import CLI
 import cv2
 import numpy as np
 import hashlib
 import time
 import os
 import random
+# zip
 import shutil
+from pathlib import Path
+
+# load the config file to get all the information
+from configparser import ConfigParser
 
 RPI_CAMERA = 0
 IMAGE_FOLDER = "Images/"
 BOX_FOLDER = "Boxes/"
 
+STEPS_FOR_FULL_CIRCLE = 12360
 
 def nothing(x):
     pass
 
 def save_to_files(bounding_box, final):
     filename = str(hashlib.md5(str.encode(str(time.time()))).hexdigest())
-    # folder for the class    
+    # folder for the class
     foldername = "new_folder/"
     # datafolder for the images and the txt files
     datafolder = foldername + "data/"
@@ -58,12 +36,45 @@ def save_to_files(bounding_box, final):
     newpath = cwd + "/" + datafolder
     if not os.path.exists(newpath):
         os.makedirs(newpath)
-    imagefile = datafolder + filename + '.png' 
+    imagefile = datafolder + filename + '.png'
     textfile = open(datafolder + filename + '.txt', 'a')
-    
+
     file_operations.save_to_folder(textfile, imagefile, bounding_box, final)
 
-    
+
+
+
+#Get the configparser object
+config_object = ConfigParser()
+
+cfg_file = Path("config.ini")
+
+if not cfg_file.exists():
+    #Assume we need 2 sections in the config file, let's call them USERINFO and SERVERCONFIG
+    config_object["GENERAL"] = {
+            "steps for full rotation": "12360",
+            "steps per image": "30"
+    }
+    config_object["DATASETINFO"] = {
+            "object0": "hose",
+            "object1": "nut",
+            "object2": "connector",
+            "object3": "shower head",
+            "object4": "bag",
+            "object5": "paper"
+    }
+
+    #Write the above sections to config.ini file
+    with open('config.ini', 'w') as conf:
+            config_object.write(conf)
+
+#Read config.ini file
+config_object.read("config.ini")
+
+#Get the password
+objects_info = config_object["DATASETINFO"]
+# Getting the entries
+objects_info["object0"]
 
 # todo get classes through GUI
 classes = []
@@ -78,7 +89,6 @@ image_processor = ImageProcessing(camera.capture())
 delay = 1/1000.0
 #images = input("How many images do you want per category (5 categories)?")
 images = 10000
-STEPS_FOR_FULL_CIRCLE = 12360
 steps = int(STEPS_FOR_FULL_CIRCLE/images)
 classes = ["Banana"]#, "Rolle"]
 
@@ -91,7 +101,7 @@ for label in classes:
     if only_train_images:
         break
     last_five_OTL_sizes = [0]*5
-        
+
     # Initialise GUI
     panel = np.zeros([100, 700], np.uint8)
     cv2.namedWindow('panel')
@@ -110,25 +120,25 @@ for label in classes:
     cv2.createTrackbar('L - v', 'panel', 0, 255, nothing)
     # 255
     cv2.createTrackbar('U - v', 'panel', 255, 255, nothing)
-    
+
     switch = '0 : OFF \n1 : ON'
     cv2.createTrackbar(switch, 'panel',0,1,nothing)
     string_motor = '0 : MOTOR OFF \n1 : MOTOR ON'
     cv2.createTrackbar(string_motor, 'panel', 0, 1, nothing)
-        
+
         # Allow user to set boundary values & get live preview
     while True:
         img_raw = camera.capture()
-    
+
         l_h = cv2.getTrackbarPos('L - h', 'panel')
         u_h = cv2.getTrackbarPos('U - h', 'panel')
         l_s = cv2.getTrackbarPos('L - s', 'panel')
         u_s = cv2.getTrackbarPos('U - s', 'panel')
         l_v = cv2.getTrackbarPos('L - v', 'panel')
-        u_v = cv2.getTrackbarPos('U - v', 'panel')          
+        u_v = cv2.getTrackbarPos('U - v', 'panel')
         start = cv2.getTrackbarPos(switch, 'panel')
         bool_motor = cv2.getTrackbarPos(string_motor, 'panel')
-        
+
         lower_green = np.array([l_h, l_s, l_v])
         upper_green = np.array([u_h, u_s, u_v])
             # for live preview
@@ -137,7 +147,7 @@ for label in classes:
         # cut out background
         img_deleted_background, mask = image_processor.remove_background_youtube(img_raw, lower_green, upper_green)
         cv2.imshow('bckgrndsgmnttn', img_deleted_background)
-        
+
         mask = cv2.blur(mask, (3, 3))
         img_gray = image_processor.gray(img_raw)
         # For possible future purposes
@@ -146,7 +156,7 @@ for label in classes:
         mask_weight = 1.0
         combined_canny_edge = image_processor.combine_edges(mask_canny_edge,
                 mask_weight, img_canny_edge, (1-mask_weight))
-        
+
         object_contour = image_processor.max_contour(combined_canny_edge)
         if key == ord('q'):
             cv2.destroyAllWindows()
@@ -172,7 +182,7 @@ for label in classes:
             cv2.imwrite('cut_out.jpg', OTL_cut_out)
             # currently: Get out of loop to start capturing
         if start == 1:
-            # Future: 
+            # Future:
             break
         if bool_motor == 1:
             motor.forward(delay, 1000)
@@ -185,9 +195,9 @@ for label in classes:
     for i in range(0, STEPS_FOR_FULL_CIRCLE, steps):
         print("Progress for " + label + ": " + str(i/STEPS_FOR_FULL_CIRCLE*100) + "%")
         img_raw = camera.capture()
-        
+
         img_deleted_background, mask = image_processor.remove_background_youtube(img_raw, lower_green, upper_green)
-        
+
         #key = cv2.waitKey(20)
             # Same as above (see preview)
         mask = cv2.blur(mask, (3, 3))
@@ -196,7 +206,7 @@ for label in classes:
         img_canny_edge = image_processor.auto_canny(img_gray)
         combined_canny_edge = image_processor.combine_edges(mask_canny_edge,
                 mask_weight, img_canny_edge, (1-mask_weight))
-                
+
         object_contour = image_processor.max_contour(combined_canny_edge)
         try:
             bounding_box = image_processor.bounding_box(object_contour)
@@ -204,7 +214,7 @@ for label in classes:
             i = i - steps
             continue
         OTL_cut_out = img_deleted_background[bounding_box[0][1]:bounding_box[1][1],bounding_box[0][0]:bounding_box[1][0],:]
-        # check if cut_out_size is 2 * lesser/greater than avg img size to prevent random outer edges to be recognized 
+        # check if cut_out_size is 2 * lesser/greater than avg img size to prevent random outer edges to be recognized
             # as an OTL
         if 0 in last_five_OTL_sizes:
         # add the current OTL cut size
@@ -223,10 +233,10 @@ for label in classes:
         if not os.path.exists("snippets/" + label):
             os.makedirs("snippets/" + label)
         filename = "snippets/" + label + "/" + str(hashlib.md5(str.encode(str(time.time()))).hexdigest()) + '.jpg'
-        
+
         cv2.imwrite(filename, OTL_cut_out)
         motor.forward(delay, int(STEPS_FOR_FULL_CIRCLE/images + 10))
-        
+
         #except Exception as e:
     #       print("I am a failure " + str(e))
     #       continue
@@ -235,8 +245,8 @@ for label in classes:
 
         # write bounding box in correct format (see file operations class)
         # write image into correct folder with correct naming
-        # 
-"""     
+        #
+"""
         filename = str(hashlib.md5(str.encode(str(time.time()))).hexdigest())
         # folder for the class
         foldername = "new_folder/"
@@ -246,13 +256,13 @@ for label in classes:
         newpath = cwd + "/" + datafolder
         if not os.path.exists(newpath):
             os.makedirs(newpath)
-        imagefile = datafolder + filename + '.png' 
+        imagefile = datafolder + filename + '.png'
         textfile = open(datafolder + filename + '.txt', 'a')
-        
+
         file_operations.save_to_folder(textfile, imagefile, bounding_box, final)
 """
         #cv2.imshow('combined', final)
-        
+
 if only_snippets:
     motor.cleanUp()
     print('finished creating snippets')
@@ -283,14 +293,14 @@ else:
             filepath = pathname + filename
             OTL_cut_out = cv2.imread(filepath)
             final, bounding_box = image_processor.OTL_on_background(OTL_cut_out)
-            if final is 0 and 0 in bounding_box:
+            if final is 0 and bounding_box is 0:
                 print("Error in OTL_on_background")
                 i = i - steps
                 continue
             save_to_files(bounding_box, final)
 
 
-        
+
         # 5 different transformations
         # 0...overlay with some image (transparent overlay)
         # 1...delete and replace the background
