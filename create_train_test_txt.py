6d43f1cda684cff1acd0d60638022b2188c0c191
andreas <andreas.besginow@hs-owl.de>
2020-10-20 15:17:12 +0200

    added script to make train and test list

--- create_train_test_txt.py
+++ create_train_test_txt.py
@@ -2,29 +2,26 @@ import shutil
 import numpy as np
 import cv2
 import glob, os
+from random import shuffle
 
+# size of validation set in percent
+validation_size = 0.2
 
 folder = 'new_folder/'
 train_txt = 'train.txt'
 valid_txt = 'test.txt'
 images_path = folder + "data/"
 
+cwd = os.getcwd()
+result = glob.glob(images_path)
+shuffle(result)
 
-all_image_files = [f for f in os.listdir(images_path) if f.endswith('.png')]
-# size of validation set in percent
-validation_size = 0.2
+with open("train.txt", 'w') as f:
+    for elem in result[:len(result)*(1-validation_size)]:
+        f.write(elem + "\n")
+with open("test.txt", 'w') as f:
+    for elem in result[len(result)*(1-validation_size):]:
+        f.write(elem + "\n")
 
-# validation
-validation_images = all_image_files[:int(len(all_image_files)*validation_size)]
 
-# training
-training_images = all_image_files[int(len(all_image_files)*validation_size):]
-
-with open(train_txt) as fp:
-    for image in training_images:
-        fp.write(image + "\n")
-
-with open(valid_txt) as fp:
-    for image in validation_images:
-        fp.write(image + "\n")
 
